5b98f1f37d37cc62104e02448219af5a2edb271c
Tauhid <tauhid.swe@gmail.com>
2022-11-29 15:51:35 +0600

    add mongodb into simple node server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
@@ -14,19 +15,58 @@ const users = [
     { id:1, name:"Sabana", email:"sabana@gmail.com" },
     { id:2, name:"Sabnoor", email:"sabnoor@gmail.com" },
     { id:3, name:"Sabila", email:"sabila@gmail.com" }
-]
+];
 
-app.get('/users', (req, res) => {
+/* 
+    username:dbuser1
+    password:cdu8rX2FeibWKn4d
+*/
+
+
+
+const uri = "mongodb+srv://dbuser1:cdu8rX2FeibWKn4d@cluster0.ybh5qdc.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const userCollection = client.db("simpleNode").collection('users');
+        
+        // const result = await userCollection.insertOne(user);
+        // console.log(result);
+
+        app.get('/users', async (req, res) =>{
+            const cursor = userCollection.find({});
+            const users = await cursor.toArray()
+            res.send(users);
+        })
+
+        app.post('/users', async (req, res) =>{
+            const user = req.body;
+
+            const result = await userCollection.insertOne(user);
+            console.log(result);
+            user._id = result.insertedId;
+            res.send(user);
+        })
+    }
+    finally{
+        
+    }
+}
+run().catch(err => console.log(err))
+
+
+/* app.get('/users', (req, res) => {
     res.send(users);
-})
+}) */
 
-app.post('/users', (req, res) =>{
+/* app.post('/users', (req, res) =>{
     const user = req.body;
     user.id = users.length + 1;
     users.push(user);
     console.log(user);
     res.send(user);
-})
+}) */
 
 app.listen(port, () =>{
     console.log(`Simplle node server is running on: ${port}`);
